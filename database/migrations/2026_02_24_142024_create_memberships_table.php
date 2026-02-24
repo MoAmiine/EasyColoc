8c0609cb41c7436ecf92d9022c67c8ae9ef85266
Moamiine <medamine.teljaoui@gmail.com>
2026-02-24 14:24:50 +0000

    create memberships table migration

---
+++ database/migrations/2026_02_24_142024_create_memberships_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('memberships', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('colocation_id')->constrained()->onDelete('cascade');
+            $table->string('role');
+            $table->datetime('joined_at');
+            $table->datetime('left_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('memberships');
+    }
+};
