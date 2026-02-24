cd9db5c54ad2d4c442ea1eb0303b85770e5df4c2
Moamiine <medamine.teljaoui@gmail.com>
2026-02-24 14:25:04 +0000

    create depenses table migration

---
+++ database/migrations/2026_02_24_142051_create_depenses_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('depenses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('colocation_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('category_id')->constrained('categories');
+            $table->string('title');
+            $table->decimal('amount', 10, 2);
+            $table->datetime('spent_at');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('depenses');
+    }
+};
