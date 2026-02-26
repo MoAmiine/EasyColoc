bd3e47c1ec12998d07686dc0514a460ae6e94027
Moamiine <medamine.teljaoui@gmail.com>
2026-02-26 16:29:20 +0000

    refactor routes to include admin dashboard and colocation management

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,37 @@
 <?php
 
+use App\Http\Controllers\ColocationController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\IsAdmin;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpKernel\HttpCache\Store;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/dashboard', function () {
+    if (Auth::user()->is_admin) {
+        return redirect()->route('admin.dashboard');
+    }
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/admin/dashboard', function () {
+    return view('admin.dashboard');
+})->middleware(['auth', 'verified', IsAdmin::class])->name('admin.dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware('auth')->group( function(){
+    Route::get('/colocations', [ColocationController::class, 'index'])->name('colocation.index');       
+    Route::get('/colocations/create', [ColocationController::class, 'create'])->name('colocation.create');
+    Route::post('/colocations/store', [ColocationController::class, 'store'])->name('colocation.store');
 });
 
 require __DIR__.'/auth.php';
