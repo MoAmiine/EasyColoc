f8b3aea6e6853c50f21bb34bc51ae763743a207d
Moamiine <medamine.teljaoui@gmail.com>
2026-03-01 18:11:28 +0000

    layout for user dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -77,7 +77,7 @@
 
                 <div class="mt-6 pt-6 border-t border-slate-50 space-y-1.5">
                     <p class="text-[10px] font-black text-slate-400 uppercase tracking-[0.2em] px-4 mb-2">Gestion</p>
-                    <a href="#" class="nav-item">
+                    <a href="{{ route('depenses.index') }}" class="nav-item">
                         <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 7h6m0 10v-3m-3 3h.01M9 17h.01M9 14h.01M12 14h.01M15 11h.01M12 11h.01M9 11h.01M7 21h10a2 2 0 002-2V5a2 2 0 00-2-2H7a2 2 0 00-2 2v14a2 2 0 002 2z"/></svg>
                         <span>Dépenses</span>
                     </a>
