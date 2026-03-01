d2ef315c4872ccf1c58aac42fe84cb1b55db4577
Moamiine <medamine.teljaoui@gmail.com>
2026-03-01 18:12:06 +0000

    added routes for depensescontroller and removed admindashboard redirection

--- routes/web.php
+++ routes/web.php
@@ -6,15 +6,16 @@ use App\Http\Middleware\IsAdmin;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Symfony\Component\HttpKernel\HttpCache\Store;
+use App\Http\Controllers\DepensesController;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Route::get('/dashboard', function () {
-    if (Auth::user()->is_admin) {
-        return redirect()->route('admin.dashboard');
-    }
+    // if (Auth::user()->is_admin) {
+    //     return redirect()->route('admin.dashboard');
+    // }
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
@@ -32,9 +33,18 @@ Route::middleware('auth')->group( function(){
     Route::get('/colocations', [ColocationController::class, 'index'])->name('colocation.index');       
     Route::get('/colocations/create', [ColocationController::class, 'create'])->name('colocation.create');
     Route::post('/colocations/store', [ColocationController::class, 'store'])->name('colocation.store');
-    Route::get('/colocations/details/{colocation}', [ColocationController::class, 'show'])->name('colocation.show');  
+    Route::get('/colocations/details/{colocation}', [ColocationController::class, 'show'])->name('colocation.show');
+    Route::get('/colocations/{collocation}/edit', [ColocationController::class, 'edit'])->name('colocation.edit');
+    Route::put('/colocations/{collocation}/update', [ColocationController::class, 'update'])->name('colocation.update');
+    Route::delete('/colocations/{colocation}/delete', [ColocationController::class, 'destroy'])->name('colocation.delete');
 });
 
+
+Route::middleware('auth')->group(function () {
+    Route::get('/depenses', [DepensesController::class, 'index'])->name('depenses.index');
+    Route::get('/depenses/create', [DepensesController::class, 'create'])->name('depenses.create');
+    Route::post('/depenses/store', [DepensesController::class, 'store'])->name('depenses.store');
+});
 
 
 require __DIR__.'/auth.php';
