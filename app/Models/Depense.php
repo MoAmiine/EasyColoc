a7465fd6aaf86db0dd7b159e144428a56fabe71c
Moamiine <medamine.teljaoui@gmail.com>
2026-03-03 11:52:08 +0000

    casted spent_at and amount types on depense model

--- app/Models/Depense.php
+++ app/Models/Depense.php
@@ -8,9 +8,19 @@ class Depense extends Model
 {
     protected $fillable = ['colocation_id', 'user_id', 'category_id', 'title', 'amount', 'spent_at'];
 
+    protected $casts = [
+        'spent_at' => 'datetime',
+        'amount' => 'decimal:2',
+    ];
+
     public function categorie()
     {
         return $this->belongsTo(Categorie::class, 'category_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
     }
 
     public function payeur()
