b4a81bac006597fff2e44098bc65a79599b59bb1
Moamiine <medamine.teljaoui@gmail.com>
2026-03-03 11:52:19 +0000

    addded invitation model

--- app/Models/Invitation.php
+++ app/Models/Invitation.php
@@ -1,10 +1,29 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Invitation extends Model
 {
-    //
+    protected $fillable = ['colocation_id', 'email', 'token', 'expires_at', 'accepted_at'];
+    
+    protected $casts = [
+        'expires_at' => 'datetime',
+        'accepted_at' => 'datetime',
+    ];
+
+    public function colocation()
+    {
+        return $this->belongsTo(Colocation::class);
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($invitation) {
+            $invitation->token = Str::random(32);
+            $invitation->expires_at = now()->addDays(7);
+        });
+    }
 }
