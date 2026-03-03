71a483fbac4cb98fd9da9047abf62ac0f1d23968
Moamiine <medamine.teljaoui@gmail.com>
2026-03-03 11:51:41 +0000

    depense request validation

--- app/Http/Requests/StoreDepenseRequest.php
+++ app/Http/Requests/StoreDepenseRequest.php
@@ -6,26 +6,30 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreDepenseRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
             'colocation_id' => 'required|exists:colocations,id',
-            'description'   => 'required|string|min:3|max:255',
+            'title'         => 'required|string|min:3|max:255',
             'amount'        => 'required|numeric|min:0.01',
             'user_id'       => 'required|exists:users,id',
+            'category_id'   => 'nullable|exists:categories,id',
+            'spent_at'      => 'required|date',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'title.required' => 'Donne un titre à cette dépense',
+            'title.min' => 'Le titre doit faire au moins 3 caractères',
+            'amount.min' => 'Le montant doit être d\'au moins 0,01 €',
+            'spent_at.required' => 'La date est obligatoire',
         ];
     }
 }
