774f50814587c1b53c116e01b1a5888ce0e75bf0
Moamiine <medamine.teljaoui@gmail.com>
2026-03-04 01:33:55 +0000

    edited paiement model

--- app/Models/Paiement.php
+++ app/Models/Paiement.php
@@ -3,18 +3,35 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Paiement extends Model
 {
-    protected $fillable = ['debtor_id', 'creditor_id', 'colocation_id', 'amount', 'is_paid'];
+    protected $fillable = [
+        'debtor_id',
+        'creditor_id',
+        'colocation_id',
+        'amount',
+        'is_paid',
+    ];
 
-    public function debiteur()
+    protected $casts = [
+        'is_paid' => 'boolean',
+        'amount' => 'decimal:2',
+    ];
+
+    public function debtor(): BelongsTo
     {
         return $this->belongsTo(User::class, 'debtor_id');
     }
 
-    public function crediteur()
+    public function creditor(): BelongsTo
     {
         return $this->belongsTo(User::class, 'creditor_id');
+    }
+
+    public function colocation(): BelongsTo
+    {
+        return $this->belongsTo(Colocation::class);
     }
 }
